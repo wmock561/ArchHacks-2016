acd93867a208afe27082bbf2849201633b4afdc2
Will <will@mockdesign.com>
2016-11-04 23:29:49 -0500

    trying to make list work

--- WEB/resources/views/index.blade.php
+++ WEB/resources/views/index.blade.php
@@ -110,6 +110,20 @@
                             </span>
                         </li>
                     </ul>
+
+                    <!--adding card-->
+                    <header class="section__play-btn mdl-cell mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone mdl-color--teal-100 mdl-color-text--white">
+                        <i class="material-icons">play_circle_filled</i>
+                    </header>
+                    <div class="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone">
+                        <div class="mdl-card__supporting-text">
+                            <h4>Message</h4> Dolore ex deserunt aute fugiat aute nulla ea sunt aliqua nisi cupidatat eu. Nostrud in laboris labore nisi amet do dolor eu fugiat consectetur elit cillum esse. TEST
+                        </div>
+                        <div class="mdl-card__actions">
+                            <a href="#" class="mdl-button">View Now</a>
+                        </div>
+                    </div>
+                    
                 </section>
             </div>
             <div class="mdl-layout__tab-panel" id="tab3Ref">
