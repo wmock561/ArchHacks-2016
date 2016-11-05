b1e3e06d59e2cf4dcce9af0dbad88b4ea6553539
Will <will@mockdesign.com>
2016-11-04 22:21:45 -0500

    removing menu option on messages

--- WEB/resources/views/feed.blade.php
+++ WEB/resources/views/feed.blade.php
@@ -3,17 +3,9 @@
 </header>
 <div class="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone">
     <div class="mdl-card__supporting-text">
-        <h4>Features</h4> Dolore ex deserunt aute fugiat aute nulla ea sunt aliqua nisi cupidatat eu. Nostrud in laboris labore nisi amet do dolor eu fugiat consectetur elit cillum esse. TEST
+        <h4>Message</h4> Dolore ex deserunt aute fugiat aute nulla ea sunt aliqua nisi cupidatat eu. Nostrud in laboris labore nisi amet do dolor eu fugiat consectetur elit cillum esse. TEST
     </div>
     <div class="mdl-card__actions">
-        <a href="#" class="mdl-button">Read our features</a>
+        <a href="#" class="mdl-button">View Now</a>
     </div>
 </div>
-<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="btn1">
-    <i class="material-icons">more_vert</i>
-</button>
-<ul class="mdl-menu mdl-js-menu mdl-menu--bottom-right" for="btn1">
-    <li class="mdl-menu__item">Lorem</li>
-    <li class="mdl-menu__item">Ipsum</li>
-    <li class="mdl-menu__item">Dolor</li>
-</ul>
