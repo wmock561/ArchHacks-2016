ba67499b9ef68c5386ce1d62eb2bea2f8c92134f
Will <will@mockdesign.com>
2016-11-04 21:04:47 -0500

    added styling

--- WEB/resources/views/index.blade.php
+++ WEB/resources/views/index.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>@yield('title')</title>
-
+    
     <!--jquery-->
     <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
 
