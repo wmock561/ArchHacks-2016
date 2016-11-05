8844b3d6efdcaeb7f4a8592e610ada979fc87218
Will <will@mockdesign.com>
2016-11-05 17:22:43 -0500

    found cdn

--- WEB/resources/views/index.blade.php
+++ WEB/resources/views/index.blade.php
@@ -21,7 +21,7 @@
     <title>StressLess</title>
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.2/locale/ar-ly.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.2/moment.js"></script>
 
     <!--jquery-->
     <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
