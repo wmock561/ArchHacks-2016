3ec1dd6b71c3bcc488b62bc3c5a19d8d7b7e08aa
Will <will@mockdesign.com>
2016-11-05 16:22:55 -0500

    changing chart

--- WEB/resources/views/vis1.blade.php
+++ WEB/resources/views/vis1.blade.php
@@ -1,29 +1,33 @@
 <script type="text/javascript">
-    google.charts.load('current', {
-        'packages': ['corechart']
-    });
-    google.charts.setOnLoadCallback(drawChart);
+        google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawSeriesChart);
 
-    function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004', 1000, 400],
-          ['2005', 1170, 460],
-          ['2006', 660, 1120],
-          ['2007', 1030, 540]
-        ]);
+    function drawSeriesChart() {
 
-        var options = {
-            title: 'Visualization 1',
-            curveType: 'linear',
-            legend: {
-                position: 'bottom'
-            }
-        };
+      var data = google.visualization.arrayToDataTable([
+        ['ID', 'Date', 'Time of Day', 'Severity', 'Duration'],
+        ['DATE', 80.66, 1.67, 1, 33739900],
+        ['DATE', 79.84, 1.36, 10, 81902307],
+        ['DATE', 78.6, 1.84, 7, 5523095],
+        ['DATE', 72.73, 2.78, 8, 79716203],
+        ['DATE', 80.05, 2, 9, 61801570],
+        ['DATE', 72.49, 1.7, 5, 73137148],
+        ['DATE', 68.09, 4.77, 4, 31090763],
+        ['DATE', 81.55, 2.96, 3, 7485600],
+        ['DATE', 68.6, 1.54, 5, 141850000],
+        ['DATE', 78.09, 2.05, 6,  307007000]
+      ]);
 
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart1'));
+      var options = {
+        title: 'Correlation between Date, Time of Day, duration and severity of Panic Attacks ',
+        hAxis: {title: 'Date'},
+        vAxis: {title: 'Time of Day'},
+        bubble: {textStyle: {fontSize: 11}},
+        colorAxis: {colors: ['green','red']}
+      };
 
-        chart.draw(data, options);
+      var chart = new google.visualization.BubbleChart(document.getElementById('chart1'));
+      chart.draw(data, options);
     }
 </script>
-<div id="curve_chart1" style="width: 900px; height: 500px"></div>
+<div id="chart1" style="width: 900px; height: 500px"></div>
