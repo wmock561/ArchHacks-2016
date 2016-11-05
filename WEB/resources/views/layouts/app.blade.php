fd5f4854925133f90ff9ee11416d6c51d973469a
Will <will@mockdesign.com>
2016-11-05 10:36:41 -0500

    changes for img

--- WEB/resources/views/layouts/app.blade.php
+++ WEB/resources/views/layouts/app.blade.php
@@ -27,55 +27,52 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-default navbar-static-top">
-        <div id="app">
-            <nav class="navbar navbar-default navbar-static-top">
-                <div class="container">
+    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top">
+           <img src="img/Asset_3.png" alt="LOGO" id="logo" height="96.3px" width="126.6px">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
 
-                    <img src="img/Asset_3.png" alt="LOGO" id="logo" height="96.3px" width="126.6px">
-                    <div class="navbar-header">
-
-                        <!-- Collapsed Hamburger -->
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                            <span class="sr-only">Toggle Navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-
-                        <!-- Branding Image -->
-                        <a class="navbar-brand" href="{{ url('/') }}">
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
                         StressLess
                     </a>
-                    </div>
-
-                    <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="nav navbar-nav">
-                            &nbsp;
-                        </ul>
-
-                        <!-- Right Side Of Navbar -->
-                        <ul class="nav navbar-nav navbar-right">
-                            <!-- Authentication Links -->
-                            @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
-                            @else
-                            <script>
-                                window.location.href = '/index';
-                            </script>
-                            @endif
-                        </ul>
-                    </div>
                 </div>
-            </nav>
 
-            @yield('content')
-        </div>
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
 
-        <!-- Scripts -->
-        <script src="/js/app.js"></script>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                        @else
+                        <script>
+                            window.location.href = '/index';
+                        </script>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="/js/app.js"></script>
 </body>
 
 </html>
