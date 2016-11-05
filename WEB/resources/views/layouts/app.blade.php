fc81e561fb064bf8f95a3d71b4365bb35b4a287c
Will <will@mockdesign.com>
2016-11-05 11:48:26 -0500

    modifying title

--- WEB/resources/views/layouts/app.blade.php
+++ WEB/resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'StressLess') }}</title>
+    <title>StressLess</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
