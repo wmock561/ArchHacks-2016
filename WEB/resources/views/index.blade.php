2da790044c36463c861d9c261b6f06b2b0ccc845
Will <will@mockdesign.com>
2016-11-05 00:33:45 -0500

    editing chart

--- WEB/resources/views/index.blade.php
+++ WEB/resources/views/index.blade.php
@@ -54,6 +54,39 @@
     <!--end added from MDl-->
 
 
+    <!--intial chart script will need to do onclick of each a tag reference-->
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {
+            'packages': ['corechart']
+        });
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2004', 1000, 400],
+          ['2005', 1170, 460],
+          ['2006', 660, 1120],
+          ['2007', 1030, 540]
+        ]);
+
+            var options = {
+                title: 'Company Performance',
+                curveType: 'linear',
+                legend: {
+                    position: 'bottom'
+                }
+            };
+
+            var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+            chart.draw(data, options);
+        }
+    </script>
+
+
 
 </head>
 
@@ -106,7 +139,7 @@
                             </span>
                         </a>
                     </aside>
-                    
+
                     <script>
                         $("#careNetwork").click(function () {
                             $('#careNetworkSection').load('/care');
