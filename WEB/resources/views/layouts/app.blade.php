9d8488123c73eb22a1b00ec33bc7eef7d244803a
Will <will@mockdesign.com>
2016-11-05 10:45:20 -0500

    trying to fix sizing

--- WEB/resources/views/layouts/app.blade.php
+++ WEB/resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.blue-light_green.min.css">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -28,7 +30,7 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
+        <nav class="navbar navbar-default">
             <div class="container">
                 <div class="navbar-header">
                     <!-- Collapsed Hamburger -->
