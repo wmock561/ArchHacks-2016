7744a02f2565d8e39ab574b229cacd3a6ae9e82d
Will <will@mockdesign.com>
2016-11-04 21:25:36 -0500

    minimizing template

--- WEB/resources/views/index.blade.php
+++ WEB/resources/views/index.blade.php
@@ -71,15 +71,15 @@
             <div class="mdl-layout--large-screen-only mdl-layout__header-row">
             </div>
             <div class="mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark">
-                <a href="#overview" class="mdl-layout__tab is-active">Overview</a>
+                <a href="#overview" class="mdl-layout__tab is-active">Feed</a>
                 <a href="#features" class="mdl-layout__tab">Features</a>
                 <a href="#features" class="mdl-layout__tab">Details</a>
                 <a href="#features" class="mdl-layout__tab">Technology</a>
                 <a href="#features" class="mdl-layout__tab">FAQ</a>
-                <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored mdl-shadow--4dp mdl-color--accent" id="add">
+                <!--button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored mdl-shadow--4dp mdl-color--accent" id="add">
                     <i class="material-icons" role="presentation">add</i>
                     <span class="visuallyhidden">Add</span>
-                </button>
+                </button>-->
             </div>
         </header>
         <main class="mdl-layout__content">
