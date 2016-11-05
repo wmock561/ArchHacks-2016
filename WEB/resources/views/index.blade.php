3bda479adb90abd9bcdfa1db80f32adfdeb98d01
Will <will@mockdesign.com>
2016-11-05 01:00:44 -0500

    pls...

--- WEB/resources/views/index.blade.php
+++ WEB/resources/views/index.blade.php
@@ -115,15 +115,13 @@
 
                         <div id="chartDiv"></div>
                         <br>
-                        <div class="mdl-layout mdl-js-layout inheritWidth">
-                                <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
-                                    <a href="#scroll-tab-1" class="mdl-layout__tab is-active">Tab 1</a>
-                                    <a href="#scroll-tab-2" class="mdl-layout__tab">Tab 2</a>
-                                    <a href="#scroll-tab-3" class="mdl-layout__tab">Tab 3</a>
-                                    <a href="#scroll-tab-4" class="mdl-layout__tab">Tab 4</a>
-                                    <a href="#scroll-tab-5" class="mdl-layout__tab">Tab 5</a>
-                                    <a href="#scroll-tab-6" class="mdl-layout__tab">Tab 6</a>
-                                </div>
+                        <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
+                            <a href="#scroll-tab-1" class="mdl-layout__tab is-active">Tab 1</a>
+                            <a href="#scroll-tab-2" class="mdl-layout__tab">Tab 2</a>
+                            <a href="#scroll-tab-3" class="mdl-layout__tab">Tab 3</a>
+                            <a href="#scroll-tab-4" class="mdl-layout__tab">Tab 4</a>
+                            <a href="#scroll-tab-5" class="mdl-layout__tab">Tab 5</a>
+                            <a href="#scroll-tab-6" class="mdl-layout__tab">Tab 6</a>
                         </div>
                     </div>
 
