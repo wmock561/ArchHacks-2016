e49b0b80c38c76acd81caaaf62549256e350d6c8
Will <will@mockdesign.com>
2016-11-04 21:35:19 -0500

    css changes

--- WEB/resources/views/index.blade.php
+++ WEB/resources/views/index.blade.php
@@ -36,7 +36,7 @@
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.deep_purple-pink.min.css">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.blue-light_green.min.css">
 
     <link rel="stylesheet" type="text/css" href="css/styles.css">
 
