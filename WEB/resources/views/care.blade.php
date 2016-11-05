7a9ff259f159786bfc3e296a53054c6d7e927d4f
Will <will@mockdesign.com>
2016-11-05 00:36:53 -0500

    changing order

--- WEB/resources/views/care.blade.php
+++ WEB/resources/views/care.blade.php
@@ -1,1 +1,29 @@
+<script type="text/javascript">
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2004', 1000, 400],
+          ['2005', 1170, 460],
+          ['2006', 660, 1120],
+          ['2007', 1030, 540]
+        ]);
+
+        var options = {
+            title: 'Company Performance',
+            curveType: 'linear',
+            legend: {
+                position: 'bottom'
+            }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+    }
+</script>
 <div id="curve_chart" style="width: 900px; height: 500px"></div>
