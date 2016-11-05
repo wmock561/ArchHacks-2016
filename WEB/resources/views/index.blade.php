a7dac369ae62dd2b9179f655a6e3724028cc3eb5
Will <will@mockdesign.com>
2016-11-05 01:40:06 -0500

    making by hand

--- WEB/resources/views/index.blade.php
+++ WEB/resources/views/index.blade.php
@@ -114,17 +114,14 @@
                     <div id="chartHolder">
 
                         <div id="chartDiv"></div>
-                        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header inheritWidth">
-                            <header class="mdl-layout__header">
-                                <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
-                                    <a href="#scroll-tab-1" class="mdl-layout__tab is-active">Tab 1</a>
-                                    <a href="#scroll-tab-2" class="mdl-layout__tab">Tab 2</a>
-                                    <a href="#scroll-tab-3" class="mdl-layout__tab">Tab 3</a>
-                                    <a href="#scroll-tab-4" class="mdl-layout__tab">Tab 4</a>
-                                    <a href="#scroll-tab-5" class="mdl-layout__tab">Tab 5</a>
-                                    <a href="#scroll-tab-6" class="mdl-layout__tab">Tab 6</a>
-                                </div>
-                            </header>
+
+                        <div>
+                            <a href="#scroll-tab-1">Tab 1</a>
+                            <a href="#scroll-tab-2">Tab 2</a>
+                            <a href="#scroll-tab-3">Tab 3</a>
+                            <a href="#scroll-tab-4">Tab 4</a>
+                            <a href="#scroll-tab-5">Tab 5</a>
+                            <a href="#scroll-tab-6">Tab 6</a>
                         </div>
                     </div>
 
