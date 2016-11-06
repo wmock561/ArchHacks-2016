108936bceb1e12715f8f94e345f1838963a2058b
Will <will@mockdesign.com>
2016-11-06 00:23:38 -0500

    testing data

--- WEB/resources/views/vis2.blade.php
+++ WEB/resources/views/vis2.blade.php
@@ -1,40 +1,47 @@
 <script type="text/javascript">
-    google.charts.load('current', {'packages': ['corechart']});
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
     google.charts.setOnLoadCallback(getData);
-    
-    function getData(){
-    
+
+    function getData() {
+
         $.get("/location", function (data) {
-            
+
             drawChart(data);
 
         });
-         
-        
-        
     }
 
-
     function drawChart(chartData) {
-        
+
         var location = new Array();
         var numCount = new Array();
-        
+
         var masterDataArray = new Array();
-        
-        masterDataArray.push(['Location','Frequency']);
-        
-        for(i=0; i<chartData.data.length; i++){
-            
-            masterDataArray.push([chartData.data[i].answer,chartData.data[i].count]);
+
+        masterDataArray.push(['Location', 'Frequency']);
+
+        for (i = 0; i < chartData.data.length; i++) {
+
+            masterDataArray.push([chartData.data[i].answer, chartData.data[i].count]);
         }
-        
-        masterDataArray.push(['other',chartData.other]);
-        
+
+        masterDataArray.push(['other', chartData.other]);
+
         console.log(masterDataArray);
+        
+        var dataTEST = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
 
         var masterData = google.visualization.arrayToDataTable([masterDataArray]);
-        
+
         console.log(masterData);
 
         var options = {
@@ -43,8 +50,7 @@
 
         var chart = new google.visualization.PieChart(document.getElementById('chart2'));
 
-        chart.draw(masterData, options);
+        chart.draw(dataTEST, options);
     }
 </script>
-
 <div id="chart2" style="width: 900px; height: 500px"></div>
