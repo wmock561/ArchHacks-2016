0370e745daa1fae5b560f336925dbef4e73168f3
Will <will@mockdesign.com>
2016-11-05 19:23:30 -0500

    checking for objects set
    may need to roll back. should be interesting

--- WEB/resources/views/index.blade.php
+++ WEB/resources/views/index.blade.php
@@ -145,12 +145,16 @@
                             <span class="mdl-list__item-primary-content">
                                 <ul class="noDot">
                                     <li>
-                                        <i class="material-icons mdl-list__item-icon">person</i>{{ $mySurveys[0]->user->personalInformation->firstName }} {{ $mySurveys[0]->user->personalInformation->lastName }}
+                                       @if ( {{ isset($mySurveys) }} )
+                                            <i class="material-icons mdl-list__item-icon">person</i>{{ $mySurveys[0]->user->personalInformation->firstName }} {{ $mySurveys[0]->user->personalInformation->lastName }}
+                                        @endif
                                     </li>
 
                                     @foreach ($surveys as $survey)
                                     <li>
-                                        <i class="material-icons mdl-list__item-icon">person</i>{{ $survey->user->personalInformation->firstName }} {{ $survey->user->personalInformation->lastName }}
+                                        @if ( {{ isset($survey) }} )
+                                            <i class="material-icons mdl-list__item-icon">person</i>{{ $survey->user->personalInformation->firstName }} {{ $survey->user->personalInformation->lastName }}
+                                        @endif
                                     </li>
                                     @endforeach
                                 </ul>
