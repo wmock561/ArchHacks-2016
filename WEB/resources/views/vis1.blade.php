9c246b4c02e3689a689dcd205176b9423f6a2583
Will <will@mockdesign.com>
2016-11-05 21:39:21 -0500

    printing for error test

--- WEB/resources/views/vis1.blade.php
+++ WEB/resources/views/vis1.blade.php
@@ -50,10 +50,14 @@
         
             //push for severity
         
-            severity.push({{ $survey->question5_answers[0]->answer }});
+            var sev = '{{ $survey->question5_answers[0]->answer }}'
+            
+            console.log(parseInt(sev));
+        
+            //severity.push({{ $survey->question5_answers[0]->answer }});
         
             //push duration
-            duration.push({{ $survey->question6_answers[0]->answer }});
+            //duration.push({{ $survey->question6_answers[0]->answer }});
         
 
         @endforeach
