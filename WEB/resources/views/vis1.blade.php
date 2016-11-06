211b2f1578479b1d970c781093f02cefcc2b14e5
Will <will@mockdesign.com>
2016-11-05 21:57:19 -0500

    pls pls work

--- WEB/resources/views/vis1.blade.php
+++ WEB/resources/views/vis1.blade.php
@@ -50,14 +50,14 @@
         
             //push for severity
         
-            var sev = '{{ $survey->question5_answers->first()}}';
+            //var sev =  $survey->question5_answers->first()';
             
             console.log(sev);
         
-            //severity.push($survey->question5_answers[0]->answer);
+            severity.push({{$survey->question5_answers[0]->answer}});
         
             //push duration
-            //duration.push($survey->question6_answers[0]->answer);
+            duration.push({{$survey->question6_answers[0]->answer}});
         
 
         @endforeach
