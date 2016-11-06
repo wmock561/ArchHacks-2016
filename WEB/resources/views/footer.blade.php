87d76dbde6119b781793ecf96f38e8092f9ecbc6
Michael Smith <mdsf3f@mail.missouri.edu>
2016-11-06 09:15:01 -0600

    Updated footer

--- WEB/resources/views/footer.blade.php
+++ WEB/resources/views/footer.blade.php
@@ -1,42 +1,40 @@
-<footer class="mdl-mega-footer">
+            <footer class="mdl-mega-footer">
                 <div class="mdl-mega-footer--middle-section">
                     <div class="mdl-mega-footer--drop-down-section">
                         <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
                         <h1 class="mdl-mega-footer--heading">Features</h1>
                         <ul class="mdl-mega-footer--link-list">
-                            <li><a href="#">About</a></li>
+                            <li><a href="#">Feed</a></li>
+                            <li><a href="#">Care Network</a></li>
+                            <li><a href="#">Settings</a></li>
                             <li><a href="/termsAndConditions">Terms</a></li>
-                            <li><a href="#">Partners</a></li>
-                            <li><a href="#">Updates</a></li>
                         </ul>
                     </div>
                     <div class="mdl-mega-footer--drop-down-section">
                         <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-                        <h1 class="mdl-mega-footer--heading">Details</h1>
+                        <h1 class="mdl-mega-footer--heading">App</h1>
                         <ul class="mdl-mega-footer--link-list">
-                            <li><a href="#">Spec</a></li>
-                            <li><a href="#">Tools</a></li>
-                            <li><a href="#">Resources</a></li>
+                            <li><a href="https://itunes.apple.com/us/genre/ios/id36?mt=8">Download</a></li>
+                            <li><a href="#">Preview</a></li>
                         </ul>
                     </div>
                     <div class="mdl-mega-footer--drop-down-section">
                         <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-                        <h1 class="mdl-mega-footer--heading">Technology</h1>
+                        <h1 class="mdl-mega-footer--heading">Resources</h1>
                         <ul class="mdl-mega-footer--link-list">
-                            <li><a href="#">How it works</a></li>
-                            <li><a href="#">Patterns</a></li>
-                            <li><a href="#">Usage</a></li>
-                            <li><a href="#">Products</a></li>
-                            <li><a href="#">Contracts</a></li>
+                            <li><a href="https://www.adaa.org/understanding-anxiety/panic-disorder-agoraphobia" target="_blank">Anxiety and Depression Society</a></li>
+                            <li><a href="www.nopanic.org.uk/resources/" target="_blank">No Panic</a></li>
+                            <li><a href="https://www.nimh.nih.gov/health/publications/panic-disorder-when-fear-overwhelms/index.shtml" target="_blank">National Institute of Mental Health</a></li>
+                            <li><a href="http://www.helpguide.org/articles/anxiety/panic-attacks-and-panic-disorders.htm" target="_blank">HelpGuide.org</a></li>
+                            <li><a href="www.webmd.com/anxiety-panic/guide/mental-health-panic-disorder" target="_blank">WebMD Panic Attacks</a></li>
                         </ul>
                     </div>
                     <div class="mdl-mega-footer--drop-down-section">
                         <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-                        <h1 class="mdl-mega-footer--heading">FAQ</h1>
+                        <h1 class="mdl-mega-footer--heading">Contact</h1>
                         <ul class="mdl-mega-footer--link-list">
-                            <li><a href="#">Questions</a></li>
-                            <li><a href="#">Answers</a></li>
-                            <li><a href="#">Contact us</a></li>
+                            <li><a href="https://dmh.mo.gov/mentalillness/progs/acimap.html" target="_blank">Access Crisis Intervention Hotline</a></li>
+                            <li><a href="http://commongroundhelps.org/" target="_blank">Common Ground</a></li>
                         </ul>
                     </div>
                 </div>
