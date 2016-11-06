88c48d7a0ee549b30e19b38b69627a39f6a4c6d7
Michael Smith <mdsf3f@mail.missouri.edu>
2016-11-06 07:49:41 -0600

    updated settings

--- WEB/resources/views/settings.blade.php
+++ WEB/resources/views/settings.blade.php
@@ -4,16 +4,21 @@
 	    <input class="mdl-textfield__input" type="text" id="sample1">
 	    <label class="mdl-textfield__label" for="sample1">Email</label>
 	  </div>
+	  <br>
 	  <p>To change your password, please fill in the boxes below.</p>
 	  <div class="mdl-textfield mdl-js-textfield">
 	    <input class="mdl-textfield__input" type="text" id="sample1">
 	    <label class="mdl-textfield__label" for="sample1">Password</label>
 	  </div>
+	  <br>
 	   <div class="mdl-textfield mdl-js-textfield">
 	    <input class="mdl-textfield__input" type="text" id="sample1">
 	    <label class="mdl-textfield__label" for="sample1">Confirm Password</label>
+
+	    <input type="submit" name="" class='mdl-textfield__input'>
 	  </div>
 	</form>
+	<br>
 	<ul>
 		<li>People who have acces</li>
 	</ul>
